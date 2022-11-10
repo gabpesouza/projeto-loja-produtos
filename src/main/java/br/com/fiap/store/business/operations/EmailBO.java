38ae6b3b3a11ae77fb31f608cf3c4a0cb230a782
Gabriel Pereira <gabriel.psouza1997@outlook.com>
2022-11-09 21:01:35 -0300

    Servico de envio de email implementado

--- src/main/java/br/com/fiap/store/business/operations/EmailBO.java
+++ src/main/java/br/com/fiap/store/business/operations/EmailBO.java
@@ -1,10 +1,48 @@
 package br.com.fiap.store.business.operations;
 
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import br.com.fiap.store.model.DB.exceptions.EmailException;
+
 public class EmailBO {
 	
 	public void enviarEmail(String destinatario, String assunto, String mensagem ) {
 		
-		final String username = "teste@gmail.com"
+		final String username = "teste@gmail.com";
+		final String senha = "senha";
+		
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable","true");
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
+		
+		Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username, senha);
+			}
+		});
+		
+		try {
+			Message email = new MimeMessage(session);
+			email.setFrom(new InternetAddress(username));
+			email.setRecipients(Message.RecipientType.TO, InternetAddress.parse(destinatario));
+			email.setSubject(assunto);
+			email.setText(mensagem);
+			
+			Transport.send(email);
+		}
+		catch(MessagingException e) {
+			throw new EmailException("Erro ao enviar email");
+		}
 		
 		
 	}
